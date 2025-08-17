f1370a1faea81ce4bf2d59e55290f25afeeac268
LocalAlbino <rudyhartwig25@outlook.com>
2025-08-17 18:46:40 -0400

    fix: server should now have code and state before client calls

--- ZRadio/Program.cs
+++ ZRadio/Program.cs
@@ -6,13 +6,14 @@ internal class Program
 {
     private static async Task Main()
     {
-        _ = Server.Redirect();
+        Server.Start();
         await Client.RequestAuthAsync();
+        Server.Redirect();
         await Client.RequestAccessTokenAsync(Server.State, Server.Code);
 
         using var timer = new Timer(TimeSpan.FromSeconds(Client.GetRefreshTimer()).TotalMilliseconds);
         timer.AutoReset = true;
-        timer.Elapsed += async (sender, e) => await Client.RequestRefreshTokenAsync();
+        timer.Elapsed += async (_, _) => await Client.RequestRefreshTokenAsync();
         timer.Enabled = true;
         timer.Start();
 
--- ZRadio/Server.cs
+++ ZRadio/Server.cs
@@ -1,27 +1,43 @@
-﻿using System.Net;
+﻿using System.Diagnostics;
+using System.Net;
 using System.Text;
 
 namespace ZRadio;
 
 internal static class Server
 {
+    private static readonly HttpListener Listener = new();
+
     public static string RedirectUri { get; } = "http://127.0.0.1:8888/callback/";
     public static string? State { get; private set; }
     public static string? Code { get; private set; }
 
-    public static async Task Redirect()
+    public static void Start()
     {
-        var server = new HttpListener();
-        server.Prefixes.Add(RedirectUri);
-        server.Start();
-        var ctx = await server.GetContextAsync();
-        var buf = Encoding.UTF8.GetBytes("<html><body>Login successful. This page can be closed now.</body></html>");
-        ctx.Response.ContentLength64 = buf.Length;
-        var queryParams = ctx.Request.QueryString;
-        await ctx.Response.OutputStream.WriteAsync(buf);
-        ctx.Response.OutputStream.Close();
-        server.Stop();
-        Code = queryParams["code"];
-        State = queryParams["state"];
+        Listener.Prefixes.Add(RedirectUri);
+        Listener.Start();
+        Console.WriteLine($"Listening on {RedirectUri}");
+    }
+
+    public static void Redirect()
+    {
+        Trace.Assert(Listener.IsListening, "Server could not start.");
+
+        var ctx = Listener.GetContext();
+        Code = ctx.Request.QueryString["code"];
+        State = ctx.Request.QueryString["state"];
+
+        var resp = Encoding.UTF8.GetBytes("<html><body>Login successful. You can close this page now.</body></html>");
+        ctx.Response.ContentLength64 = resp.Length;
+        ctx.Response.ContentType = "text/html";
+        ctx.Response.StatusCode = 200;
+        ctx.Response.OutputStream.Write(resp, 0, resp.Length);
+
+        Listener.Stop();
+    }
+
+    private static bool IsCodeOrStateNull()
+    {
+        return Code == null || State == null;
     }
 }
